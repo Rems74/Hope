cb3e3e1132ee416091ebecf6af13243c1f617ee2
Rems74 <92451539+Rems74@users.noreply.github.com>
2022-05-24 11:54:39 +0200

    canopée

--- src/SceneMenu.js
+++ src/SceneMenu.js
@@ -8,8 +8,9 @@ class SceneMenu extends Phaser.Scene{
         this.load.image('play', 'assets/menu/bouton-play.png');
         this.load.image('play2', 'assets/menu/bouton-play-2.png');
         this.load.image('titre', 'assets/menu/titre.png');
-        this.load.image('menu', 'assets/menu/menu2.png');
-        this.load.image('etoiles', 'assets/menu/etoiles.png');
+        this.load.image('menu', 'assets/menu/Menu3.png');
+        this.load.image('etoiles', 'assets/menu/etoiles menu.png');
+        this.load.image('lune', 'assets/menu/lune menu.png');
     }
 
     create() {
@@ -17,11 +18,12 @@ class SceneMenu extends Phaser.Scene{
 
         const ui = this.add.image(0, 0, 'menu').setOrigin(0, 0).setPipeline('Light2D');
         this.add.image(0, 0, 'etoiles').setOrigin(0, 0);
+        this.add.image(0, 0, 'lune').setOrigin(0, 0);
 
 
-        let playbutton = this.add.image(960, 580, 'play').setPipeline('Light2D');
+        let playbutton = this.add.image(1000, 650, 'play').setPipeline('Light2D');
 
-        this.add.image(680, 150, 'titre').setOrigin(0, 0);
+        this.add.image(770, 250, 'titre').setOrigin(0, 0);
 
         this.lights.enable().setAmbientColor(0x555555);
 
