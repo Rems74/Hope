ace92135527c6b1788ffff02f57b704c0f7b2f36
Rems74 <92451539+Rems74@users.noreply.github.com>
2022-05-30 13:21:24 +0200

    la fin la

--- src/SceneVictoire.js
+++ src/SceneVictoire.js
@@ -5,10 +5,10 @@ class SceneVictoire extends Phaser.Scene {
     }
 
     preload() {
-        for (let v = 1; v <= 8; v++) {
-            this.load.image('v' + v, 'assets/anim/victoire/v' + v + '.png')
+        for (let v = 1; v <= 3; v++) {
+            this.load.image('v' + v, 'assets/anim/victoire/end' + v + '.png')
         }
-        this.load.image('merci', 'assets/images/merci.png');
+        //this.load.image('merci', 'assets/images/merci.png');
     }
 
     create() {
@@ -18,31 +18,31 @@ class SceneVictoire extends Phaser.Scene {
             key: 'plebe',
             frames: [
                 {key: 'v1'},
+                {key: 'v1'},
+                {key: 'v1'},
+                {key: 'v2'},
+                {key: 'v2'},
                 {key: 'v2'},
                 {key: 'v3'},
-                {key: 'v4'},
-                {key: 'v5'},
-                {key: 'v6'},
-                {key: 'v7'},
-                {key: 'v8'},
+
             ],
-            frameRate: 4,
+            frameRate: 1,
             repeat: 0
         });
         this.anims = this.add.sprite(0,0,'plebe').setOrigin(0,0);
         this.anims.play('plebe');
 
-        this.merci=this.add.image(680, 330, 'merci').setOrigin(0, 0);
+        //this.merci=this.add.image(680, 330, 'merci').setOrigin(0, 0);
 
-        this.tweens.add({
-            targets: this.merci,
-            alphaTopLeft: { value: 1, duration: 5000, ease: 'Power1' },
-            alphaBottomRight: { value: 1, duration: 10000, ease: 'Power1' },
-            alphaBottomLeft: { value: 1, duration: 5000, ease: 'Power1', delay: 5000 },
-            yoyo: false,
-            loop: 0
+        // this.tweens.add({
+        //     targets: this.merci,
+        //     alphaTopLeft: { value: 1, duration: 5000, ease: 'Power1' },
+        //     alphaBottomRight: { value: 1, duration: 10000, ease: 'Power1' },
+        //     alphaBottomLeft: { value: 1, duration: 5000, ease: 'Power1', delay: 5000 },
+        //     yoyo: false,
+        //     loop: 0
 
-        });
+       // });
 
 
     }
